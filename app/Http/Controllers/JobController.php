63ad4321ccd7513d7b4aacc24b8588af481864d5
Phan Thanh Hoàng <hoangphandev98@gmail.com>
2024-03-24 17:03:48 +0700

    Implement search function

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -6,53 +6,82 @@ use App\Models\Job;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
-class JobController extends Controller {
+class JobController extends Controller
+{
     /**
      * Display a listing of the resource.
      */
-    public function index(): View {
-        return view('job.index', ['jobs' => Job::all()]);
+    public function index(): View
+    {
+        $jobs = Job::query();
+
+        $jobs
+            ->when(request('search'), function ($query) {
+                $query->where(function ($query) {
+                    $query->where('title', 'like', '%' . request('search') . '%')->orWhere('description', 'like', '%' . request('search') . '%');
+                });
+            })
+            ->when(request('minSalary'), function ($query) {
+                $query->where('salary', '>=', request('minSalary'));
+            })
+            ->when(request('maxSalary'), function ($query) {
+                $query->where('salary', '<=', request('maxSalary'));
+            })
+            ->when(request('experience'), function ($query) {
+                $query->where('experience', request('experience'));
+            })
+            ->when(request('category'), function ($query) {
+                $query->where('category', request('category'));
+            });
+
+        return view('job.index', ['jobs' => $jobs->get()]);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create() {
+    public function create()
+    {
         //
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         //
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Job $job) {
+    public function show(Job $job)
+    {
         return view('job.show', ['job' => $job]);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id) {
+    public function edit(string $id)
+    {
         //
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id) {
+    public function update(Request $request, string $id)
+    {
         //
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id) {
+    public function destroy(string $id)
+    {
         //
     }
 }
--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,6 +1,43 @@
 <x-layout>
     <x-breadcrumbs :links="['Jobs' => route('jobs.index')]"></x-breadcrumbs>
 
+    <x-card class="mb-4 text-sm">
+        <form action="{{ route('jobs.index') }}" method="GET">
+            <div class="mb-4 grid grid-cols-2 gap-4">
+                <div>
+                    <div class="mb-1 font-semibold">
+                        Search
+                    </div>
+                    <x-text-input name="search" value="{{ request('search') }}" placeholder="Search for any text" />
+                </div>
+                <div>
+                    <div class="mb-1 font-semibold">
+                        Salary
+                    </div>
+                    <div class="flex space-x-2">
+                        <x-text-input name="minSalary" value="{{ request('minSalary') }}" placeholder="From" />
+                        <x-text-input name="maxSalary" value="{{ request('maxSalary') }}" placeholder="To" />
+                    </div>
+                </div>
+                <div>
+                    <div class="mb-1 font-semibold">
+                        Experience
+                    </div>
+
+                    <x-radio-group name="experience" :options="\App\Models\Job::$experience"/>
+                </div>
+                <div>
+                    <div class="mb-1 font-semibold">
+                        Category
+                    </div>
+
+                    <x-radio-group name="category" :options="\App\Models\Job::$categories"/>
+                </div>
+            </div>
+
+            <button class="w-full">Filters</button>
+        </form>
+    </x-card>
 
     @foreach ($jobs as $job)
         <x-job-card :$job>
